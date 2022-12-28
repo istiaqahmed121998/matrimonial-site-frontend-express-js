847423c12e17f4c8432c2b9a09b4a432ddca6d4e
Md Istiaq Ahmed <istiaqahmed121998@gmail.com>
2022-12-29 03:39:20 +0600

    check

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+console.log(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,process.env.DB_HOST)
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
